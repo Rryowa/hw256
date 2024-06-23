7469a390501d2466cf7c931107bef934df362ae0
Rryowa <rryowa@gmail.com>
2024-06-23 18:26:24 +0300

    Strategy behavioral pattern

--- internal/service/package/package.go
+++ internal/service/package/package.go
@@ -5,50 +5,11 @@ import (
 	"homework/internal/util"
 )
 
-<<<<<<< HEAD
-// TODO: add LRU cache using Strategy pattern
-type packageContext struct {
-	strategies map[models.PackageType]PackageStrategy
-}
-
-=======
->>>>>>> dc762e3 (refactor into command pattern)
 type PackageService interface {
 	ValidatePackage(weight models.Weight, packageType models.PackageType) error
 	ApplyPackage(order *models.Order, packageType models.PackageType)
 }
 
-<<<<<<< HEAD
-type PackageStrategy interface {
-	Validate(weight models.Weight) error
-	Apply(order *models.Order)
-}
-
-func NewPackageService() PackageService {
-	return &packageContext{
-		strategies: map[models.PackageType]PackageStrategy{
-			FilmType:   NewFilmPackage(),
-			PacketType: NewPacketPackage(),
-			BoxType:    NewBoxPackage(),
-		},
-	}
-}
-
-func (pc *packageContext) ValidatePackage(weight models.Weight, packageType models.PackageType) error {
-	if strategy, ok := pc.strategies[packageType]; ok {
-		return strategy.Validate(weight)
-	}
-	return util.ErrPackageTypeInvalid
-}
-
-func (pc *packageContext) ApplyPackage(order *models.Order, packageType models.PackageType) {
-	if strategy, ok := pc.strategies[packageType]; ok {
-		strategy.Apply(order)
-		return
-	}
-	//Assume that film has no weight limit
-	pc.strategies[FilmType].Apply(order)
-=======
 type packageService struct {
 	filmPackage   *FilmPackage
 	packetPackage *PacketPackage
@@ -96,5 +57,4 @@ func (ps *packageService) ApplyPackage(order *models.Order, packageType models.P
 	case "":
 		ps.filmPackage.Apply(order)
 	}
->>>>>>> dc762e3 (refactor into command pattern)
 }
