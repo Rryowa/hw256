33475e77e41f7be74222a3d7dc0bf501779beeab
Rryowa <rryowa@gmail.com>
2024-06-29 22:42:02 +0300

    unit test refactor

--- internal/service/order_test.go
+++ internal/service/order_test.go
@@ -1,1 +1,20 @@
 package service
+
+import (
+	"github.com/stretchr/testify/require"
+	"homework/internal/models"
+	"homework/mocks"
+	"testing"
+)
+
+func (uts *UnitTestSuite) Test_Exists() {
+	uts.T().Run("Test_Exists", func(t *testing.T) {
+		t.Parallel()
+		repo := mocks.NewMockStorage(t)
+		orderService := NewOrderService("public", repo, uts.packageService)
+		repo.EXPECT().Get("1", "public").Return(models.Order{}, nil)
+
+		_, exists := orderService.Exists("1")
+		require.Equal(t, true, exists)
+	})
+}
--- internal/service/validation_test.go
+++ internal/service/validation_test.go
@@ -2,55 +2,12 @@ package service
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 	"homework/internal/models"
 	"homework/internal/util"
 	"homework/mocks"
-	"strconv"
 	"testing"
 	"time"
 )
-
-type UnitTestSuite struct {
-	suite.Suite
-	orderService      *mocks.MockOrderService
-	packageService    *mocks.MockPackageService
-	validationService ValidationService
-	schemaName        string
-
-	inputDto      models.Dto
-	expectedOrder models.Order
-}
-
-func TestUnitTestSuite(t *testing.T) {
-	suite.Run(t, new(UnitTestSuite))
-}
-
-func (uts *UnitTestSuite) SetupTest() {
-	uts.orderService = mocks.NewMockOrderService(uts.T())
-	uts.packageService = mocks.NewMockPackageService(uts.T())
-	uts.validationService = NewValidationService(uts.orderService, uts.packageService)
-	uts.schemaName = "public"
-	dto := models.Dto{
-		ID:           "1",
-		UserID:       "1",
-		StorageUntil: "2077-01-01",
-		OrderPrice:   "999.99",
-		Weight:       "10",
-		PackageType:  "box",
-	}
-	uts.inputDto = dto
-	storageUntilDate, _ := time.Parse(time.DateOnly, dto.StorageUntil)
-	orderPriceFloat, _ := strconv.ParseFloat(dto.OrderPrice, 64)
-	weightFloat, _ := strconv.ParseFloat(dto.Weight, 64)
-	uts.expectedOrder = models.Order{
-		ID:           dto.ID,
-		UserID:       dto.UserID,
-		StorageUntil: storageUntilDate,
-		OrderPrice:   models.Price(orderPriceFloat),
-		Weight:       models.Weight(weightFloat),
-	}
-}
 
 func Test_Validate_IsEmpty(t *testing.T) {
 	assert.Equal(t, true, isArgEmpty(""))
